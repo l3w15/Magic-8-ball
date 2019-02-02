6b7e7fcce68b6fedbfb5325a1c3b63db28b6b24f
lewiswatson <lewis.watson@bp.com>
2019-02-02 18:46:08 +0000

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Lewis Watson on 02/02/2019.
+//  Copyright © 2019 Lewis Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
